e8b5997a2afe4effb7b8fd1d212b9c2bd0231543
Joe Gesualdo <joegesualdo@gmail.com>
2012-12-06 14:03:18 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startupthecity::Application.config.secret_token = 'b5a3f3e7816699af0b398d363a1bf61df02597a17a7450ff3690642a02708f1f54cc78c9580777627709b47988de0ff2c1600c566974ceda6bed1c57077f9954'
